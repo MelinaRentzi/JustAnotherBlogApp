29a362e641ef32c6f38d2d5861492f2828c2efee
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-03 06:49:27 +0000

    first commit/initialization

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var app = express();
+
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started!");
+});
