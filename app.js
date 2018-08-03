d2f48d402bd48d1e96b1a60816166c243cba0f77
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-03 06:57:11 +0000

    Mongoose Configuration

--- app.js
+++ app.js
@@ -3,10 +3,25 @@ var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var app = express();
 
+//App config
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+//Mongoose/Model Config
+var blogSchema = new mongoose.Schema({
+   title: String,
+   image: {type: String, default: "https://i0.wp.com/www.revenuearchitects.com/wp-content/uploads/2017/02/Blog_pic.png?resize=300%2C170&ssl=1"},
+   body: String,
+   created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTful Routes
+
 
 
 
