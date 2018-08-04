32adc255262dff39304772280297377cd3284841
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-04 09:53:11 +0000

    Added DELETE functionality

--- app.js
+++ app.js
@@ -97,6 +97,18 @@ app.put("/blogs/:id", function(req, res){
     });
 });
 
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    // destroy blog and redirect
+    Blog.findByIdAndRemove(req.params.id, function(err){
+       if(err){
+           res.redirect("/blogs");
+       } else {
+           res.redirect("/blogs");
+       }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has started!");
 });
