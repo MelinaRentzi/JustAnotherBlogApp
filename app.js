ecc78b93e83e0d5980912cef69e763045e5de365
Melina Rentzi <r.melina_92@hotmail.com>
2018-08-04 10:29:52 +0000

    Added express-sanitizer and worked on index page with semantic-ui

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 var mongoose = require("mongoose");
 var app = express();
 
@@ -10,6 +11,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer()); // needs to be after bodyParser
 
 
 //Mongoose/Model Config
@@ -52,6 +54,7 @@ app.get("/blogs/new", function(req, res){
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
    //create blog
+   req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.create(req.body.blog, function(err, newBlog){
        if(err){
            res.render("new");
@@ -88,6 +91,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
        if(err){
            res.redirect("/blogs");
